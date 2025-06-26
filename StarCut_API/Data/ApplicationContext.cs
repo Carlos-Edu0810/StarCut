75563b38d1617979152cc0da4ef71a9e23f0cae9
Carlos-Edu0810 <ramos0810eduardo@gmail.com>
2025-06-26 00:05:45 -0300

    feat: Configura a camada de acesso a dados com Entity Framework Core

---
+++ StarCut_API/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace StarCut_API.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptionsBuilder optionsBuilder)
+        {
+                  
+        }
+    }
+}
